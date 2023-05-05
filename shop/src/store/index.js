09e84b18b44cd6bbb29302a46dc23cffa8c8eb2b
huanghao <756788268@qq.com>
2023-05-05 13:13:32 +0800

    修改初始余额、修改扣款顺序判断

--- shop/src/store/index.js
+++ shop/src/store/index.js
@@ -13,7 +13,7 @@ export default new Vuex.Store({
     msg: [], //被点击商品信息
     cartShop: ["1"], //购物车商品
     orders: [], //订单数据
-    money: 0, //余额
+    money: 10000, //余额
     waits: [], //存储已完成待发货的订单数据
     time: [], //存储所有订单时间
     noarr: [], // 未支付订单存储
@@ -22,226 +22,226 @@ export default new Vuex.Store({
   getters: {
     //localStorage获取
     //登录token
-    logintoken(state) {
+    logintoken (state) {
       if (state.token == null || state.token.length < 1) {
-        state.token = Cookies.get("token");
+        state.token = Cookies.get("token")
         if (!state.token) {
-          state.token = localStorage.getItem("token");
+          state.token = localStorage.getItem("token")
         }
 
       }
-      return state.token;
+      return state.token
     },
     //产品详情
-    getmsg(state) {
+    getmsg (state) {
       if (state.msg == null || state.msg.length < 1) {
-        state.msg = JSON.parse(localStorage.getItem("msg"));
+        state.msg = JSON.parse(localStorage.getItem("msg"))
       }
-      return state.msg;
+      return state.msg
     },
     //购物车信息
-    getcart(state) {
+    getcart (state) {
       // console.log(state.cartShop);
       if (state.cartShop == null || state.cartShop.length < 2) {
-        state.cartShop = JSON.parse(localStorage.getItem("cartShop"));
+        state.cartShop = JSON.parse(localStorage.getItem("cartShop"))
         if (!state.cartShop) {
           state.cartShop = ["1"]
         }
       }
       // console.log(state.cartShop);
-      return state.cartShop;
+      return state.cartShop
     },
     //获取订单信息
-    getOrders(state) {
+    getOrders (state) {
       if (state.orders == null || state.orders.length < 1) {
-        state.orders = JSON.parse(localStorage.getItem("orders"));
+        state.orders = JSON.parse(localStorage.getItem("orders"))
       }
-      return state.orders;
+      return state.orders
     },
     // 获取余额
-    getmoney(state) {
+    getmoney (state) {
       if (state.money == null || state.money < 1) {
-        state.money = Number(JSON.parse(localStorage.getItem("money")));
+        state.money = Number(JSON.parse(localStorage.getItem("money")))
         if (state.money <= 0 || state.money == null) {
-          state.money = 88888;
-          localStorage.setItem("money", JSON.stringify(state.money));
+          state.money = 88888
+          localStorage.setItem("money", JSON.stringify(state.money))
         }
       }
-      return state.money;
+      return state.money
     },
     // 获取待发货数据
-    getsend(state) {
+    getsend (state) {
       if (state.waits == null || state.waits.length < 1) {
-        state.waits = JSON.parse(localStorage.getItem("waits"));
+        state.waits = JSON.parse(localStorage.getItem("waits"))
       }
-      return state.waits;
+      return state.waits
     },
     //获取订单时间
-    getTime(state) {
+    getTime (state) {
       if (state.time == null || state.time.length < 1) {
-        state.time = JSON.parse(localStorage.getItem("time"));
+        state.time = JSON.parse(localStorage.getItem("time"))
       }
-      return state.time;
+      return state.time
     },
     //获取未支付
-    getno(state) {
+    getno (state) {
       if (state.noarr == null || state.noarr.length < 1) {
-        state.noarr = JSON.parse(localStorage.getItem("noarr"));
+        state.noarr = JSON.parse(localStorage.getItem("noarr"))
       }
-      return state.noarr;
+      return state.noarr
     },
     //获取退款
-    getrefund(state) {
+    getrefund (state) {
       if (state.refund == null || state.refund.length < 1) {
-        state.refund = JSON.parse(localStorage.getItem("refund"));
+        state.refund = JSON.parse(localStorage.getItem("refund"))
       }
-      return state.refund;
+      return state.refund
     }
 
   },
   mutations: {
     //保存登录token
-    saveToken(state, payload) {
-      state.token = payload;
+    saveToken (state, payload) {
+      state.token = payload
       Cookies.set("token", payload, {
         expires: 1
       }) //把token存储cookies里时间为一天
-      localStorage.setItem("token", payload);
+      localStorage.setItem("token", payload)
     },
     //删除登录token
-    removeToken(state) {
-      state.token = null;
-      Cookies.remove("token");
-      localStorage.removeItem("token");
+    removeToken (state) {
+      state.token = null
+      Cookies.remove("token")
+      localStorage.removeItem("token")
     },
     //产品详情
-    msg(state, payload) {
-      state.msg = payload;
-      localStorage.setItem("msg", JSON.stringify(payload));
+    msg (state, payload) {
+      state.msg = payload
+      localStorage.setItem("msg", JSON.stringify(payload))
     },
     //加入购物车
-    addcart(state, payload) {
-      let key = false;
+    addcart (state, payload) {
+      let key = false
       // console.log(state.cartShop);
       if (state.cartShop) {
         if (state.cartShop.length > 1) {
           state.cartShop.forEach((v) => {
             if (v.id == payload.id) {
-              key = true;
+              key = true
             }
           })
         }
       }
 
-      if (key) return;
-      state.cartShop.unshift(payload);
+      if (key) return
+      state.cartShop.unshift(payload)
       // console.log(state.cartShop);
-      localStorage.setItem("cartShop", JSON.stringify(state.cartShop));
+      localStorage.setItem("cartShop", JSON.stringify(state.cartShop))
     },
     //清空购物车
-    removecart(state) {
-      localStorage.removeItem("cartShop");
-      state.cartShop = ["1"];
-      localStorage.setItem("cartShop", JSON.stringify(state.cartShop));
+    removecart (state) {
+      localStorage.removeItem("cartShop")
+      state.cartShop = ["1"]
+      localStorage.setItem("cartShop", JSON.stringify(state.cartShop))
     },
     //删除单个商品
-    delecart(state, payload) {
-      state.cartShop = JSON.parse(localStorage.getItem("cartShop"));
+    delecart (state, payload) {
+      state.cartShop = JSON.parse(localStorage.getItem("cartShop"))
       for (let i = 0; i < state.cartShop.length; i++) {
         if (state.cartShop[i].id == payload.id) {
-          state.cartShop.splice(i, 1);
-          localStorage.setItem("cartShop", JSON.stringify(state.cartShop));
-          return;
+          state.cartShop.splice(i, 1)
+          localStorage.setItem("cartShop", JSON.stringify(state.cartShop))
+          return
         }
       }
     },
     //加入订单
-    order(state, payload) {
+    order (state, payload) {
       if (state.orders == null || state.orders.length < 1) {
-        state.orders = payload;
+        state.orders = payload
         state.orders = Array(state.orders)
-        localStorage.setItem("orders", JSON.stringify(state.orders));
+        localStorage.setItem("orders", JSON.stringify(state.orders))
       } else {
-        state.orders.push(payload);
-        localStorage.setItem("orders", JSON.stringify(state.orders));
+        state.orders.push(payload)
+        localStorage.setItem("orders", JSON.stringify(state.orders))
       }
     },
     //清空订单数据
-    removeOrder(state) {
+    removeOrder (state) {
       state.orders = [],
-        localStorage.setItem("orders", JSON.stringify(state.orders));
+        localStorage.setItem("orders", JSON.stringify(state.orders))
     },
     //消费余额
-    paymoney(state, payload) {
-      state.money = Number(state.money) - payload;
+    paymoney (state, payload) {
       if (state.money < 0) {
-        Toast.fail("余额不足，支付失败 !");
-        return;
+        Toast.fail("余额不足，支付失败 !")
+        return
       }
-      localStorage.setItem("money", JSON.stringify(state.money));
+      state.money = Number(state.money) - payload
+      localStorage.setItem("money", JSON.stringify(state.money))
     },
     //存储已完成待发货的订单数据
-    waitSend(state, payload) {
+    waitSend (state, payload) {
       if (state.waits == null || state.waits.length < 1) {
         if (payload.nopay != null || payload.nopay == true) {
           state.waits = payload
         } else {
-          state.waits = Array(payload);
+          state.waits = Array(payload)
         }
         // if (typeof state.waits != Array) {
         //   state.waits = Array(state.waits);
         // }
-        localStorage.setItem("waits", JSON.stringify(state.waits));
+        localStorage.setItem("waits", JSON.stringify(state.waits))
       } else {
-        state.waits.unshift(payload);
-        localStorage.setItem("waits", JSON.stringify(state.waits));
+        state.waits.unshift(payload)
+        localStorage.setItem("waits", JSON.stringify(state.waits))
       }
     },
     //存储订单时间
-    storageTime(state, payload) {
+    storageTime (state, payload) {
       if (state.time == null || state.time.length < 1) {
-        state.time = payload;
+        state.time = payload
         if (typeof state.time != Array) {
-          state.time = Array(state.time);
+          state.time = Array(state.time)
         }
-        localStorage.setItem("time", JSON.stringify(state.time));
+        localStorage.setItem("time", JSON.stringify(state.time))
       } else {
-        state.time.unshift(payload);
-        localStorage.setItem("time", JSON.stringify(state.time));
+        state.time.unshift(payload)
+        localStorage.setItem("time", JSON.stringify(state.time))
       }
     },
     // 未支付订单存储
-    noPay(state, payload) {
+    noPay (state, payload) {
       if (state.noarr == null || state.noarr.length < 1) {
-        state.noarr = payload;
+        state.noarr = payload
         if (typeof state.noarr != Array) {
-          state.noarr = Array(state.noarr);
+          state.noarr = Array(state.noarr)
         }
-        localStorage.setItem("noarr", JSON.stringify(state.noarr));
+        localStorage.setItem("noarr", JSON.stringify(state.noarr))
       } else {
-        state.noarr.unshift(payload);
-        localStorage.setItem("noarr", JSON.stringify(state.noarr));
+        state.noarr.unshift(payload)
+        localStorage.setItem("noarr", JSON.stringify(state.noarr))
       }
     },
     // 删除未支付订单
-    removepay(state, payload) {
-      state.noarr.splice(payload, 1);
-      localStorage.setItem("noarr", JSON.stringify(state.noarr));
+    removepay (state, payload) {
+      state.noarr.splice(payload, 1)
+      localStorage.setItem("noarr", JSON.stringify(state.noarr))
     },
     //退款
-    removeShop(state, payload) {
+    removeShop (state, payload) {
       if (state.refund == null || state.refund.length < 1) {
-        state.refund = Array(state.waits[payload]);
-        localStorage.setItem("refund", JSON.stringify(state.refund));
+        state.refund = Array(state.waits[payload])
+        localStorage.setItem("refund", JSON.stringify(state.refund))
       } else {
-        state.refund.unshift(state.waits[payload]);
-        localStorage.setItem("refund", JSON.stringify(state.refund));
+        state.refund.unshift(state.waits[payload])
+        localStorage.setItem("refund", JSON.stringify(state.refund))
       }
 
-      state.waits.splice(payload, 1);
-      state.time.splice(payload, 1);
-      localStorage.setItem("waits", JSON.stringify(state.waits));
-      localStorage.setItem("time", JSON.stringify(state.time));
+      state.waits.splice(payload, 1)
+      state.time.splice(payload, 1)
+      localStorage.setItem("waits", JSON.stringify(state.waits))
+      localStorage.setItem("time", JSON.stringify(state.time))
     },
   },
   actions: {},
